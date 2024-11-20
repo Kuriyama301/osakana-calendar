adec52b726841fd059967477cecd2c101d6022f6
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-11-20 18:01:00 +0900

    feature/#157-add-loading-screen-test

--- front/app/src/components/Common/__tests__/LoadingScreen.test.jsx
+++ front/app/src/components/Common/__tests__/LoadingScreen.test.jsx
@@ -1,1 +1,24 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import LoadingScreen from "../LoadingScreen";
 
+describe("LoadingScreen", () => {
+  it("表示状態のテスト", () => {
+    render(<LoadingScreen isOpen={true} />);
+
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+    expect(
+      screen.getByText("旬のサカナの情報を読み込んでいます...")
+    ).toBeInTheDocument();
+  });
+
+  it("非表示状態のテスト", () => {
+    render(<LoadingScreen isOpen={false} />);
+
+    expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("旬のサカナの情報を読み込んでいます...")
+    ).not.toBeInTheDocument();
+  });
+});

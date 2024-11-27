8c0150545f76ebb815c0b4143a84902086b1b1b1
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-11-27 19:25:51 +0900

    feature/#68-add-youtube-test

---
+++ api/test/controllers/api/v1/youtube_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class Api::V1::YoutubeControllerTest < ActionDispatch::IntegrationTest
+  test "検索キーワードで動画を取得できる" do
+    mock_response = {
+      success: true,
+      data: [{ id: "video1", title: "サンプル動画" }]
+    }
+
+    YoutubeService.stub :search, mock_response do
+      get "/api/v1/youtube/search", params: { q: "マグロ" }
+
+      assert_response :success
+      assert_equal 1, json_response["videos"].length
+    end
+  end
+end
--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -1,6 +1,8 @@
+# api/test/test_helper.rb
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/mock"  # モック機能のために追加
 
 class ActiveSupport::TestCase
   # フィクスチャをセットアップ
@@ -9,5 +11,26 @@ class ActiveSupport::TestCase
   # 必要に応じて共通のヘルパーメソッドを追加
   def json_response
     JSON.parse(@response.body)
+  end
+
+  # YouTubeのモックレスポンスを生成するヘルパー
+  def mock_youtube_success_response
+    {
+      success: true,
+      data: [
+        {
+          id: "test_video_id",
+          title: "テスト動画",
+          thumbnail_url: "http://example.com/thumbnail.jpg"
+        }
+      ]
+    }
+  end
+
+  def mock_youtube_error_response
+    {
+      success: false,
+      error: "動画の検索に失敗しました"
+    }
   end
 end

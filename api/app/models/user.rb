2ef3aed1fb35ebb2d2aecdaeb1af67faa694da3d
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-12-01 15:10:02 +0900

    feature/#213-email-confirmation

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   require 'devise'
 
   # Deviseの機能を追加
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :trackable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable, :confirmable, :trackable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   # バリデーション
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
 
-  # JWTトークンのペイロードをカスタマイズ
+  # JWTトークンのペイロードをカスタマイズ (必要であれば実装)
   def jwt_payload
     {
       'email' => email,
---
+++ api/db/migrate/20241201013529_add_confirmable_to_users.rb
@@ -0,0 +1,4 @@
+class AddConfirmableToUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end

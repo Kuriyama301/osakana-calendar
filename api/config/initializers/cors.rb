38bd7794981f078dd46b239fabc76bf12364c43d
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-12-17 14:23:22 +0900

    fix/#247-add-gem-action cable-v2

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,12 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:5173')
+    origins ENV.fetch('ALLOWED_ORIGINS', 'http://localhost:5173').split(',')
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization', 'Content-Type'],
-      credentials: ENV.fetch('CORS_CREDENTIALS', 'true') == 'true',
-      max_age: 3600
+      credentials: ENV.fetch('CORS_CREDENTIALS', 'false') == 'true',
+      expose: [
+        'Access-Control-Allow-Origin',
+        'Authorization', # JWTトークンのための認証ヘッダー
+        'X-CSRF-Token' # CSRFトークン
+      ]
   end
 end

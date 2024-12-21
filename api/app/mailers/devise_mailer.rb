9f7c772ec5bacd2783ff280efc4d67283094ed42
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-12-21 16:29:52 +0900

    feature/#249-add-password-reset-for-api

--- api/app/mailers/devise_mailer.rb
+++ api/app/mailers/devise_mailer.rb
@@ -16,4 +16,19 @@ class DeviseMailer < Devise::Mailer
 
     super
   end
+
+  # パスワードリセット用のメソッドを追加
+  def reset_password_instructions(record, token, opts={})
+    @token = token
+    @resource = record
+    @frontend_url = Rails.env.production? ?
+      ENV.fetch('FRONTEND_URL', 'https://www.osakana-calendar.com') :
+      'http://localhost:5173'
+
+    opts[:subject] = 'パスワード再設定の手順'
+    opts[:from] = ENV.fetch('MAILER_FROM', 'info@osakana-calendar.com')
+    opts[:reply_to] = ENV.fetch('MAILER_FROM', 'info@osakana-calendar.com')
+
+    super
+  end
 end

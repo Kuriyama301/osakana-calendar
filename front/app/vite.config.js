c8f9d17307d997fb9429928c3a64c3994d24e779
Kuriyama301 <kuriyama.kosuke@gmail.com>
2024-12-31 16:56:49 +0900

    feature/#188-add-debug-product-google-auth-v3

--- front/app/vite.config.js
+++ front/app/vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
     "import.meta.env.VITE_FRONT_URL": JSON.stringify(
       process.env.VITE_FRONT_URL
     ),
+    "import.meta.env.VITE_GOOGLE_CLIENT_ID": JSON.stringify(
+      process.env.VITE_GOOGLE_CLIENT_ID
+    ),
+    "import.meta.env.VITE_YOUTUBE_API_KEY": JSON.stringify(
+      process.env.VITE_YOUTUBE_API_KEY
+    ),
   },
 
   test: {

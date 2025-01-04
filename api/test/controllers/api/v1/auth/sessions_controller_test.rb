93035461f2bc86129eba7d87e346464d475b6aae
Kuriyama301 <kuriyama.kosuke@gmail.com>
2025-01-04 23:19:23 +0900

    feature/#288-add-auth-test

---
+++ api/test/controllers/api/v1/auth/sessions_controller_test.rb
@@ -0,0 +1,70 @@
+require "test_helper"
+
+class Api::V1::Auth::SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:user)  # 既存のfixtureを使用
+  end
+
+  test "should sign in confirmed user" do
+    post api_v1_user_session_path,
+      params: {
+        user: {
+          email: @user.email,
+          password: 'password'  # fixtureに合わせたパスワード
+        }
+      },
+      as: :json
+
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_equal 'success', json['status']
+    assert json['token'].present?
+  end
+
+  test "should not sign in unconfirmed user" do
+    unconfirmed_user = users(:unconfirmed_user)
+    post api_v1_user_session_path,
+      params: {
+        user: {
+          email: unconfirmed_user.email,
+          password: 'password'
+        }
+      },
+      as: :json
+
+    assert_response :unauthorized
+  end
+
+  test "should not sign in with invalid password" do
+    post api_v1_user_session_path,
+      params: {
+        user: {
+          email: @user.email,
+          password: 'wrong_password'
+        }
+      },
+      as: :json
+
+    assert_response :unauthorized
+  end
+
+  test "should sign out user" do
+    # ログイン
+    post api_v1_user_session_path,
+      params: {
+        user: {
+          email: @user.email,
+          password: 'password'
+        }
+      },
+      as: :json
+
+    token = JSON.parse(response.body)['token']
+
+    # ログアウト
+    delete destroy_api_v1_user_session_path,
+      headers: { 'Authorization' => token }
+
+    assert_response :success
+  end
+end
